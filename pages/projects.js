a40fdd0a17435517ba9b60be603eceb923cd0af6
GitHubJIYEON <jiyeon2954@gmail.com>
2023-04-12 18:01:01 +0900

    api +

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,7 @@
 import Layout from '../components/layout'
 import Head from 'next/head'
+import { TOKEN, DATABASE_ID } from '../config'
+
 export default function Projects() {
   return (
     <>
@@ -13,4 +15,27 @@ export default function Projects() {
       </Layout>
     </>
   )
+}
+
+//빌드 타임에 호출이 됨
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  }
+
+  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err))
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
